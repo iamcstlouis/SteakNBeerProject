ceb634146604d56ab9669cded9cba0d0f2c8c09a
Chantal St Louis <misscstlouis@gmail.com>
2019-10-23 22:53:17 +0100

    Made a start on updating Hamburger Menu

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@ function openSlideMenu() {
 function closeSlideMenu() {
     document.getElementById('side-menu').style.width = '0';
 }
+
+
+
+/* ===================================================================
+            Hamburger Toggler Animation
+=================================================================== */
+
+const hamburgerBtn = document.querySelector('.menu-btn');
+// const hamburgerToggler = document.querySelector('.hamburger-toggler');
+let menuOpen = false;
+
+hamburgerBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        hamburgerBtn.classList.add('open');
+        // hamburgerToggler.classList.add('open');
+        menuOpen = true;
+    } else {
+        hamburgerBtn.classList.remove('open');
+        // hamburgerBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});

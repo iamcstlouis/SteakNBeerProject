b772eb9dcd378f75186a7140eaefe20145c12ecb
Chantal St Louis <misscstlouis@gmail.com>
2019-10-26 01:35:59 +0100

    comments removed in JS Main file

--- js/main.js
+++ js/main.js
@@ -2,16 +2,9 @@
             Hamburger Toggler Animation
 =================================================================== */
 
-// const menuBtn = document.querySelector('.menu-btn');
-// const hamburgerToggler = document.querySelector('.hamburger-toggler');
-// const lineTop = document.querySelector('.line-top');
-// const menu = document.querySelector('.menu');
-// const lineBtm = document.querySelector('.line-btm');
-
 const selectElement = (element) => document.querySelector(element);
 
 selectElement('.hamburger-btn').addEventListener('click', () => {
-    // selectElement('.menu-btn').classList.toggle('open');
     selectElement('.hamburger-toggler').classList.toggle('open');
     selectElement('.mobile-menu').classList.toggle('open');
     selectElement('.line-top').classList.toggle('open');
